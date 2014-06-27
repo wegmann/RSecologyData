c8f61e9d73e7b5821cfd49930c0ed2ffbcb4fdf6
Martin Wegmann <martin.wegmann@remote-sensing-biodiversity.org>
2014-06-27 16:42:07 +0200

    added more data

--- R/load_vector.R
+++ R/load_vector.R
@@ -1,50 +1,43 @@
-#' @title load all vector files for RS eco 
+#' @title load all vector files stored in the RSecologyData package
 #' 
-#' @description kurzbeschreibung
+#' @description All files are spatial vector data sets (Polygons, Lines or Points) providing information 
+#' such as border of protected areas, field sampling sites or roads. 
 #' @keywords shape vector, spatial data frames
-#' @details details
+#' @details the files are
+#' lsat_extent_LL: landsat extent for path 224 r63 (geographic coordinates lat long)
+#' pa: protected areas of the area covered by lsat_extent (projection UTM north WGS84)
+#' roads: roads of the area covered by lsat_extent (projection UTM north WGS84)
 #' @export
-#' @seealso loadraster()
+#' @seealso load_raster()
 #' @examples
-#' loadvector()
+#' load_vector()
 #' plot(lsat_extent_LL)
+#' ##
+#' plot(pa)
+#' plot(abspres_poi,col="yellow",add=T)
+#' @references
+#' \href{http://remote-sensing-biodiversity.org/rsecology-book}.
 #' @author Martin Wegmann
 
-
-loadvector <- function(overwrite=TRUE){
-  if(exists("lsat_extent_LL")) warning("file 'lsat_extent_LL' already exists",call.=FALSE)
-  else(
-  lsat_extent_LL<<-readRDS(system.file("extdata/lsat_extent_LL.rds",package="rsecodata"))
-  )
-}
-
-
-#' Worldwide Reference System - 2 (WRS2) SpatialPolygonsDataFrame
-#' 
-#' Contains polygons of the area of each path and row for the ascending and 
-#' descending nodes of the WRS2 system.
-#'
-#' @docType data
-#' @name wrs2_asc_desc
-#' @seealso wrs1_asc_desc
-#' @references
-#' \href{http://landsat.usgs.gov/tools_wrs-2_shapefile.php}{WRS-2 Shape files 
-#' courtesy of the U.S. Geological Survey}.
-NULL
-
-
+        load_vector <- function(overwrite=TRUE){
+#                               x <- c("lsat_extent_LL","lsat_extent","modis_extent","roads","pa","abspres_poi","pres_poi","count_poi","trend_poi","study_area")
+                              x <- c("lsat_extent_LL","roads","pa","abspres_poi","pres_poi","count_poi","trend_poi")
+                              
+                              for ( i in x) {
+                              if(exists(i)) warning('object "', i, '" already exists',call.=FALSE)
+                              else(
+                              assign(i,readRDS(system.file(paste("extdata/",i,".rds",sep=""),package="RSecologyData")),envir=.GlobalEnv)
+                              )
+                              }
+            }
+    
 #' data: vector file with the extent of the Landsat scene path 224 r 63 - SpatialPolygonsDataFrame
 #' 
 #' Contains a polygon of the area of the Landsat scene with path 224 and row 63 of the WRS2 system.
 #'
 #' @docType data
-#' @name lsat_extent_LL
-#' @seealso wrs2_asc_desc
-#' @examples
-#' plot(lsat_extent_LL)
-#' @references
-#' \href{http://landsat.usgs.gov/tools_wrs-2_shapefile.php}{WRS-1 Shape files 
-#' courtesy of the U.S. Geological Survey}.
+#' @name load_vector
+
 NULL
 
 
